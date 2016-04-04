dfedcc8315d8bd1ae05cf46f0656c084e7d8417d
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2016-04-04 18:18:38 -0400

    Working on adding ways to both, upload and download files

--- reflect-client.py
+++ reflect-client.py
@@ -1,26 +1,26 @@
 import os,sys,argparse
 import json
-import requests
 import subprocess
 import hashlib
-
+import urllib, urllib2
 import logging
 try:
 	from pycommons import generic_logging
 	generic_logging.init(level=logging.INFO)
 except:
-	logging.basicconfig(level=logging.DEBUG)
+	logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__file__)
 
 def process(server, port, outdir=''):
 	# Get the json
 	base_url = server + ':' + str(port)
 	logger.debug("Requesting server for reflection")
-	req = requests.get(base_url + '/reflect')
-	logger.debug("Server responds with status code: %d" % (req.status_code))
+	url = base_url + '/reflect'
+	req = urllib2.urlopen(url)
+	#logger.debug("Server responds with status code: %d" % (req.status_code))
 
 	# Process it
-	reflection = req.json()
+	reflection = json.loads(req.read())
 
 	for target, attr in reflection.iteritems():
 		logger.debug("Processing target: " + target)
@@ -33,35 +33,46 @@ def process(server, port, outdir=''):
 			else:
 				logger.info("\tSuccessfully processed packages target '%s'" % (target))
 		elif attr['type'] == 'file':
-			attr = dict(attr)
-			path = os.path.join(outdir, attr['path'])
-			path = path.replace('~', os.environ['HOME'])
-			# Check if we have a version of this file
-			if os.path.exists(path):
-				attr['hash'] = hashlib.sha256(open(path, 'r').read()).hexdigest()
-			# Set ID to be target
-			attr['id'] = target
-			req = requests.get(base_url + '/file', params=attr)
-			# Check response
-			response = req.json()
-			if response['match'] is True:
-				# We have nothing to do
-				logger.debug("\tHashes match!")
-				logger.info("\tSuccessfully processed target '%s'" % (target))
-				continue
-			try:
-				directory = os.path.dirname(path)
-				if not os.path.exists(directory):
-					os.makedirs(directory)
-			except Exception, e:
-				logger.warn("\tFailed to makedirs(): %s" % (e.message))
+			func = globals()[attr['method']]
+			func(base_url, target, attr, outdir)
 
-			try:
-				with open(path, 'w') as f:
-					f.write(req.text)
-				logger.info("\tSuccessfully processed target '%s' to file: %s'" % (target, path))
-			except Exception, e:
-				logger.error("\tFailed to write file: %s" % (e.message))
+def download(base_url, target, attr, outdir):
+		attr = dict(attr)
+		path = os.path.join(outdir, attr['path'])
+		path = path.replace('~', os.environ['HOME'])
+		# Check if we have a version of this file
+		if os.path.exists(path):
+			attr['hash'] = hashlib.sha256(open(path, 'r').read()).hexdigest()
+		# Set ID to be target
+		attr['id'] = target
+		url_qs = urllib.urlencode(attr)
+		url = base_url + '/file' + '?' + url_qs
+		logger.info('GET ' + url)
+		req = urllib2.urlopen(url)
+		# Check response
+		response = json.loads(req.read())
+		if response['match'] is True:
+			# We have nothing to do
+			logger.debug("\tHashes match!")
+			logger.info("\tSuccessfully processed target '%s'" % (target))
+			return
+		try:
+			directory = os.path.dirname(path)
+			if not os.path.exists(directory):
+				os.makedirs(directory)
+		except Exception, e:
+			logger.warn("\tFailed to makedirs(): %s" % (e.message))
+
+		try:
+			with open(path, 'w') as f:
+				f.write(response['data'])
+			logger.info("\tSuccessfully processed target '%s' to file: %s'" % (target, path))
+		except Exception, e:
+			logger.error("\tFailed to write file: %s" % (e.message))
+			import pdb
+			pdb.set_trace()
+			print ''
+
 
 
 
